9578334a891e76cd3ff88b1b8b71949d77eb320f
vvisionnn <wz546500392@gmail.com>
2020-09-22 15:51:46 +0800

    reorder some code

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div id="nav">
+      <router-link to="/">Home</router-link> | 
+      <router-link to="/about">About</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
